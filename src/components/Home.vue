0da550d582876df8cf3b1e3cdd83761068691675
Justin Wilkey <justin.wilkey@tentkeep.com>
2018-01-13 12:15:16 -0500

    Initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.home {
+}
+</style>
