20af4ba5664c956740276ce9583dc33f5fbe7a02
Justin Wilkey <justin.wilkey@tentkeep.com>
2018-01-13 23:48:24 -0500

    Add Titlebar to contact page

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,33 +1,39 @@
 <template>
-  <div class="contact pad">
-    <div class="theme-mid pad rounded shadow marginb">
-      <h1>Contact Tentkeep</h1>
-      <p class="marginb">Please use the form below to contact our representatives, and we will get back to you promptly.</p>
+  <div class="contact">
+    <titlebar></titlebar>
 
-      <br />
-      <div v-if="messageSent" class="marginb">
-        <p>Thank You! Your message has been sent.</p>
-        <router-link to="/" class="callout alt">Return to home page</router-link>
-      </div>
-      <div class="flex-column">
-        <div id="content" @click="startTyping" @blur="stopEditing" contenteditable="true" class="shadow-inset pad marginb rounded">
-          <p v-if="!editing && !content" class="muted">type your request or questions here...</p>
+    <div class="pad">
+      <div class="theme-mid pad rounded shadow marginb">
+        <h1>Contact Tentkeep</h1>
+        <p class="marginb">Please use the form below to contact our representatives, and we will get back to you promptly.</p>
+
+        <br />
+        <div v-if="messageSent" class="marginb">
+          <p>Thank You! Your message has been sent.</p>
+          <router-link to="/" class="callout alt">Return to home page</router-link>
         </div>
-      </div>
-      <div class="flex-row marginb">
-        <div class="flex-one"></div>
-        <button @click="send">SEND</button>
-      </div>
-      <br />
+        <div class="flex-column">
+          <div id="content" @click="startTyping" @blur="stopEditing" contenteditable="true" class="shadow-inset pad marginb rounded">
+            <p v-if="!editing && !content" class="muted">type your request or questions here...</p>
+          </div>
+        </div>
+        <div class="flex-row marginb">
+          <div class="flex-one"></div>
+          <button @click="send">SEND</button>
+        </div>
+        <br />
 
-      <div class="hi-bottom marginb"></div>
+        <div class="hi-bottom marginb"></div>
 
-      <p class="marginb">Thank you for your interest in working with Tentkeep. The products we create are sure to impress you as well as your audience. We will help you discover and build the perfect solution for your needs.</p>
+        <p class="marginb">Thank you for your interest in working with Tentkeep. The products we create are sure to impress you as well as your audience. We will help you discover and build the perfect solution for your needs.</p>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import Titlebar from '@/components/Titlebar'
+
 export default {
   name: 'contact',
   data () {
@@ -37,6 +43,7 @@ export default {
       messageSent: false
     }
   },
+  components: { Titlebar },
   methods: {
     startTyping () {
       this.editing = true
---
+++ src/components/Titlebar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="titlebar flex-row align-center">
+    <img @click="goHome" src="../../static/images/logo.png" />
+    <p @click="goHome" class="marginl">TENTKEEP</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'titlebar',
+  data () {
+    return {
+    }
+  },
+  methods: {
+    goHome () {
+      this.$router.push('/')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.titlebar {
+  padding: 8px;
+  img {
+    cursor: pointer;
+    height: 30px;
+  }
+}
+</style>
